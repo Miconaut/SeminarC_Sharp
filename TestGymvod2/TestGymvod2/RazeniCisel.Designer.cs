2cf0d1b40fa8aea8bbc096db44682fba01fda800
Miconaut <71273636+Miconaut@users.noreply.github.com>
2021-01-08 23:48:22 +0100

    Add files via upload

---
+++ TestGymvod2/TestGymvod2/RazeniCisel.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TestGymvod2
+{
+    partial class RazeniCisel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Vstup = new System.Windows.Forms.TextBox();
+            this.Vysledek = new System.Windows.Forms.TextBox();
+            this.Sort = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Vstup
+            // 
+            this.Vstup.Location = new System.Drawing.Point(274, 110);
+            this.Vstup.Name = "Vstup";
+            this.Vstup.Size = new System.Drawing.Size(355, 20);
+            this.Vstup.TabIndex = 0;
+            // 
+            // Vysledek
+            // 
+            this.Vysledek.Location = new System.Drawing.Point(274, 214);
+            this.Vysledek.Name = "Vysledek";
+            this.Vysledek.ReadOnly = true;
+            this.Vysledek.Size = new System.Drawing.Size(355, 20);
+            this.Vysledek.TabIndex = 1;
+            // 
+            // Sort
+            // 
+            this.Sort.Location = new System.Drawing.Point(402, 164);
+            this.Sort.Name = "Sort";
+            this.Sort.Size = new System.Drawing.Size(75, 23);
+            this.Sort.TabIndex = 2;
+            this.Sort.Text = "Seřadit";
+            this.Sort.UseVisualStyleBackColor = true;
+            this.Sort.Click += new System.EventHandler(this.Sort_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(402, 270);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 3;
+            this.Reset.Text = "Resetovat";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Zadejte libovolná čísla oddělená čárkou bez mezery:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Seřazená čísla:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(303, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Řazení čísel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Sort);
+            this.Controls.Add(this.Vysledek);
+            this.Controls.Add(this.Vstup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Razeni cisel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Vstup;
+        private System.Windows.Forms.TextBox Vysledek;
+        private System.Windows.Forms.Button Sort;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TestGymvod2/TestGymvod2/RazeniCisel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGymvod2
+{
+    public partial class RazeniCisel : Form
+    {
+        public static int[] cisla;
+        public RazeniCisel()
+        {
+            InitializeComponent();
+        }
+
+        private void Sort_Click(object sender, EventArgs e)
+        {
+            cisla = Vstup.Text.Split(',').Select(s => int.Parse(s)).ToArray();
+
+
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                int temp;
+
+                for (int deleni = 0; deleni < cisla.Length - 1; deleni++)
+                {
+                    if (cisla[deleni] > cisla[deleni + 1])
+                    {
+                        temp = cisla[deleni + 1];
+                        cisla[deleni + 1] = cisla[deleni];
+                        cisla[deleni] = temp;
+                    }
+                }
+            }
+
+            string[] cisla2 = cisla.Select(x => x.ToString()).ToArray();
+            Vysledek.Text = string.Join(",", cisla2);
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Vstup.Text = String.Empty;
+            Vysledek.Text = String.Empty;
+        }
+    }
+}
